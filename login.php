b894bd918441f7f7e8a8250516f0b4bb82be9b0e
CaRdIaC14 <sebastian.eps98@gmail.com>
2021-12-20 03:03:45 -0300

    foto

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     include_once 'conexion.php';
     $usuario=$_POST["usuario"];
     $contrasenia=$_POST["contrasenia"];
-    $sql="SELECT correo,count(correo) FROM usuario WHERE correo ='$usuario' and contrasenia = '$contrasenia'";
+    $sql="SELECT correo,count(correo),fotoPerfil FROM usuario WHERE correo ='$usuario' and contrasenia = '$contrasenia'";
     $resultado=$conexion->query($sql);
     $fila = $resultado->fetch_row();
     if ($fila[1] == 0) {
@@ -11,6 +11,7 @@
     else{
         session_start();
         $_SESSION["USUARIO"] = $usuario;
+        $_SESSION["FOTO"] =$fila[2];
         header ("location: menuLogin.php");
     }
 ?>
